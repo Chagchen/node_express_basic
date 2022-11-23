9f71770b4aaa985814c1429bcdf0db12dc22b6a0
chagchen <chagchen@outlook.com>
2022-11-22 22:01:54 -0800

    first commit

---
+++ Users.js
@@ -0,0 +1,26 @@
+const users = [{
+        id: 1,
+        name: 'Ben',
+        email: 'ben@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Sam',
+        email: 'sam@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Jen',
+        email: 'jen@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Brad',
+        email: 'brad@mail.com'
+    },
+    
+
+]
+
+module.exports = users;
+
